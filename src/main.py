2a53dfbf7306d3e186e1be1786436f0b0a660878
laabsjus <laabsjus@hu-berlin.de>
2023-06-11 17:57:07 +0200

    Added function to display useful information about a specific event log

--- src/main.py
+++ src/main.py
@@ -1,15 +1,40 @@
 import pm4py
 
-def get_event_log(path: str):
+def get_log(path: str):
     return pm4py.read_xes(path)
 
-def save_dfg_of_event_log(event_log):
-    dfg, start_activities, end_activities = pm4py.discovery.discover_dfg(event_log, 
+def print_log_info(log):
+    start_activities = pm4py.stats.get_start_activities(log)
+    end_activities = pm4py.stats.get_end_activities(log)
+    event_attributes = pm4py.stats.get_event_attributes(log)
+    trace_attributes = pm4py.stats.get_trace_attributes(log)
+#    event_attribute_values = [
+#        {attribute: pm4py.stats.get_event_attribute_values(log, attribute)}
+#        for attribute in event_attributes]
+#    trace_attribute_values = [
+#        {attribute: pm4py.stats.get_trace_attribute_values(log, attribute)}
+#        for attribute in trace_attributes
+#    ]
+#    variants = pm4py.stats.get_variants(log)
+
+    info = f"Event log information:\n"
+    info += f"Start activities: {start_activities}\n"
+    info += f"End activities: {end_activities}\n"
+    info += f"Event attributes: {event_attributes}\n"
+    info += f"Trace attributes: {trace_attributes}\n"
+#    info += f"Event attribute values: {event_attribute_values}\n"
+#    info += f"Trace attribute values: {trace_attribute_values}\n"
+#    info += f"Variants: {variants}"
+    print(info)
+
+def save_dfg_of_log(log):
+    dfg, start_activities, end_activities = pm4py.discovery.discover_dfg(log, 
                                                                          case_id_key= "case:concept:name",
                                                                          activity_key= "concept:name")
     pm4py.vis.save_vis_dfg(dfg, start_activities, end_activities, file_path="../out/dfg/sepsis.svg")
 
 if __name__ == "__main__":
-    event_log = get_event_log("../logs/sepsis_event_log.xes")
-    save_dfg_of_event_log(event_log)
+    log = get_log("../logs/sepsis_event_log.xes")
+    print_log_info(log)
+    #save_dfg_of_log(log)
     
