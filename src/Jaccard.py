19be5e1ea5e2dbc7f33b8dba32a245ab53e32f08
laabsjus <laabsjus@hu-berlin.de>
2023-10-09 21:51:15 +0200

    Added N_grams for weighted Jaccard

--- src/Jaccard.py
+++ src/Jaccard.py
@@ -433,3 +433,221 @@ class Jaccard_N_grams:
         # perform hierarchical clustering and generate the resulting dendrogram
         self.linkage = Clustering.create_linkage(self.distance_matrix.to_numpy())
         self.dendrogram = Clustering.create_dendrogram(self.activities, self.linkage, ax=ax, no_plot=no_plot)
+
+class Weighted_Jaccard_N_grams:
+    def __init__(self, log):
+        self.log = log
+        self.connections_df = None
+        self.G = None
+        self.distance_matrix = None
+        self.activities = None
+        self.linkage = None
+        self.dendrogram = None
+        self.clusterings = None
+        self.hierarchies = None
+
+    def get_log(self):
+        return self.log
+
+    def get_connections_df(self):
+        return self.connections_df
+
+    def get_G(self):
+        return self.G
+
+    def get_distance_matrix(self):
+        return self.distance_matrix
+
+    def get_activities(self):
+        return self.activities
+
+    def get_linkage(self):
+        return self.linkage
+
+    def get_dendrogram(self):
+        return self.dendrogram
+
+    def get_clusterings(self):
+        return self.clusterings
+
+    def get_hierarchies(self):
+        return self.hierarchies
+    
+    def get_predecessors_len_n(self, curr_node, length):
+        """
+        Recursively finds all variants of paths of length n that have an outgoing link at the last node to curr_node.
+        In other words: If there is a path from a node A to curr_node of length n, then it is the predecessor_paths list
+        
+        Every solution always includes curr_node.
+        Solution adapted from: https://stackoverflow.com/a/28103735
+        """
+        
+        # Base case: Return a list with one list item curr_node
+        if length == 0:
+            return [[curr_node]]
+        
+        # the result list
+        predecessor_paths = []
+        
+        # recursively generate predecessors_paths
+        for neighbor in self.G.predecessors(curr_node):
+            for path in self.get_predecessors_len_n(neighbor, length-1):
+                # add curr_node only if it is not already in there, loops don't count
+                if curr_node not in path:
+                    predecessor_paths.append(path + [curr_node])
+        
+        return predecessor_paths
+
+    def get_successors_len_n(self, curr_node, length):
+        """
+        Recursively finds all variants of paths of length n that have an ingoing link at the first node from curr_node.
+        In other words: If there is a path from curr_node to a node A of length n, then it is the successor_paths list
+        
+        Every solution always includes curr_node.
+        Solution adapted from: https://stackoverflow.com/a/28103735
+        """
+
+        # Base case: Return a list with one list item curr_node
+        if length == 0:
+            return [[curr_node]]
+        
+        # the result list
+        successor_paths = []
+
+        # recursively generate successors_path
+        for neighbor in self.G.successors(curr_node):
+            for path in self.get_successors_len_n(neighbor, length-1):
+                # add curr_node only if it is not already in there, loops don't count
+                if curr_node not in path:
+                    successor_paths.append([curr_node] + path)
+
+        return successor_paths
+
+    def get_neighbours(self, curr_node, length):
+        """
+        Get all paths of length n that lead to start_node and start from start_node and join them together
+        """
+
+        # get predecessor and descendant paths of start_node 
+        paths_pred = self.get_predecessors_len_n(curr_node, length)
+        paths_succ = self.get_successors_len_n(curr_node, length)
+
+        # convert the nested list into list of tuples and remove the start_node
+        paths_pred = [tuple(path[:-1]) for path in paths_pred]
+        paths_succ = [tuple(path[1:]) for path in paths_succ]
+
+        # convert to sets
+        paths_pred = set(paths_pred)
+        paths_succ = set(paths_succ)
+
+        # join predecessors and descendants and remove duplicates
+        paths = set(paths_pred).union(set(paths_succ))
+
+        return paths
+
+    def get_weighted_jaccard_distance(self, node1, node2, length):
+        """
+        Determine the Weighted Jaccard similarity between the two nodes. Multiplies the weight of the nodes in a path
+        and then calculates the similarity similar to Weighted_Jaccard's similarity calculation method
+        """
+
+        # skip evaluation step if the nodes are the same
+        if node1 == node2:
+            return 1
+
+        # get neighbours of node1 and node2
+        node1_neighbours = self.get_neighbours(node1, length)
+        node2_neighbours = self.get_neighbours(node2, length)
+
+        # handle edge cases where there were no paths of length n found
+        if len(node1_neighbours) == 0 or len(node2_neighbours) == 0:
+            return 0 
+
+        # convert the lists to sets
+        node1_neighbours = set(node1_neighbours)
+        node2_neighbours = set(node2_neighbours)
+
+        # find the intersection of the sets
+        intersection = node1_neighbours.intersection(node2_neighbours)
+
+        minimumns = 0
+        maximums = 0
+
+        # multiply every connection in a path and use this to calculate the weighted Jaccard similarity between the nodes
+        for path in intersection:
+            node1_path_weight = 1
+            node2_path_weight = 1
+
+            # start nodes
+            node1_from_node = node1
+            node2_from_node = node2
+            for to_node in path:
+                # multiply the edge weight to nodeX_path_weight
+                node1_path_weight *= self.connections_df[to_node][node1_from_node]
+                node2_path_weight *= self.connections_df[to_node][node2_from_node]
+               
+                # change the start node in path to get right edge weight in next iteration
+                node1_from_node = to_node
+                node2_from_node = to_node
+
+            # sum up all the weights with minimal and maximal value
+            minimumns += min(node1_path_weight, node2_path_weight)
+            maximums += max(node1_path_weight, node2_path_weight)
+        
+        # prevent division by 0 error
+        if maximums == 0:
+            return 0
+
+        return minimumns / maximums
+
+    def get_weighted_jaccard_distance_matrix(self, activities, length):
+        """
+        Computes the Weighted Jaccard distance between all pairs of labels and converts them to a distance matrix
+        """
+
+        jaccard_table = {} # the distances between the nodes
+
+        # calculate the weighted jaccard distances between all pairs of nodes and save them in jaccard_table
+        for node1 in activities:
+            node1_distances = {}
+            for node2 in activities:
+                similarity = self.get_weighted_jaccard_distance(node1, node2, length)
+                node1_distances[node2] = round(1 - similarity, 6)
+            jaccard_table[node1] = node1_distances
+
+        jaccard_table = pd.DataFrame(jaccard_table)
+        
+        return jaccard_table
+
+    def perform_clustering(self, length, verbose=False, ax=None, no_plot=False):
+        '''
+        Performs all necessary steps to perform hierarchical clustering using Weighted Jaccard with n_grams
+        and then generating a hierarchy used for abstracting the event log
+        '''
+
+        # convert event log to an undirected NetworkX graph
+        dfg, start, end = pm4py.discover_dfg(self.log)
+        edge_list = [(edge[0], edge[1], weight) for edge, weight in dfg.items()]
+        self.G = nx.DiGraph()
+        self.G.add_weighted_edges_from(edge_list)
+        
+        # display the dfg of the given log
+        if verbose:
+            Log_processing.show_dfg_of_log(self.log)
+
+        # convert the log to a DataFrame 
+        self.connections_df = Log_processing.get_pivot_df_from_dfg(self.log)
+        self.connections_df = Log_processing.get_weighted_df(self.connections_df)
+        self.activities = self.connections_df.columns
+        
+        # generate the distance matrix
+        self.distance_matrix = self.get_weighted_jaccard_distance_matrix(self.activities, length)
+        
+        # print the distances between events
+        if verbose:
+            print(self.distance_matrix)        
+            
+        # perform hierarchical clustering and generate the resulting dendrogram
+        self.linkage = Clustering.create_linkage(self.distance_matrix.to_numpy())
+        self.dendrogram = Clustering.create_dendrogram(self.activities, self.linkage, ax=ax, no_plot=no_plot)
+        
