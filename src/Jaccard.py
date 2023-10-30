dce273a8e5c5028952c382e8ef8fbb2e38f121b4
laabsjus <laabsjus@hu-berlin.de>
2023-10-30 16:14:48 +0100

    Rewrote Simple Jaccard with own implementation, for more code consistency, and because the other implementation can't generate Jaccard with directed graphs

--- src/Jaccard.py
+++ src/Jaccard.py
@@ -15,85 +15,100 @@ class Simple_Jaccard:
         self.dendrogram = None
         self.clusterings = None
         self.hierarchies = None
+
+    def get_neighbours(self, curr_node):
+        """
+        Get all predecessors and successors of curr_node and join them together
+        """
+
+        # get predecessors and successors
+        preds = self.G.predecessors(curr_node)
+        succs = self.G.successors(curr_node)
+
+        # convert to set for union operation
+        preds = set(preds)
+        succs = set(succs)
+
+        # neighbours are the union of those sets
+        neighbours = preds.union(succs)
+
+        return neighbours
+
+    def get_jaccard_distance(self, node1, node2):
+        """
+        Determine the Jaccard similarity between the two nodes.
+        """
+
+        # skip evaluation step if the nodes are the same
+        if node1 == node2:
+            return 1
+
+        # get neighbours of node1 and node2
+        node1_neighbours = self.get_neighbours(node1)
+        node2_neighbours = self.get_neighbours(node2)
+
+        # handle edge cases where there were no paths of length n found
+        if len(node1_neighbours) == 0 or len(node2_neighbours) == 0:
+            return 0 
+
+        # convert the lists to sets
+        node1_neighbours = set(node1_neighbours)
+        node2_neighbours = set(node2_neighbours)
+
+        # find the intersection and union of the sets
+        intersection = node1_neighbours.intersection(node2_neighbours)
+        union = node1_neighbours.union(node2_neighbours)
+
+        return len(intersection) / len(union)
+
+    def get_jaccard_distance_matrix(self, activities):
+        """
+        Computes the Jaccard distance between all pairs of labels and converts them to a distance matrix
+        """
+
+        jaccard_table = {} # the distances between the nodes
+
+        # calculate the jaccard distances between all pairs of nodes and save them in jaccard_table
+        for node1 in activities:
+            node1_distances = {}
+            for node2 in activities:
+                similarity = self.get_jaccard_distance(node1, node2)
+                node1_distances[node2] = round(1 - similarity, 6)
+            jaccard_table[node1] = node1_distances
+
+        jaccard_table = pd.DataFrame(jaccard_table)
         
-    def get_jaccard_distance_of_dfg(self):
-        '''
-        Computes the jaccard measure of two nodes using the DFG
-
-        NetworkX provides an implementation of the jaccard measure in directly-follows-graphs.
-        First the DFG has to be generated using pm4py. The output gets formatted for a NetworkX graph.
-        Output is an iterator of the pairwise distances of the nodes in the graph
-        '''
-
-        # convert event log to an undirected NetworkX graph
-        dfg, start, end = pm4py.discover_dfg(self.log)
-        edge_list = [(edge[0], edge[1], weight) for edge, weight in dfg.items()]
-        dg = nx.DiGraph()
-        dg.add_weighted_edges_from(edge_list)
-
-        # compute the jaccard measures of the nodes
-        distances = nx.jaccard_coefficient(dg.to_undirected(), ebunch=dg.edges)
-
-        return distances
-
-    def convert_distances_to_distance_matrix(self, pairwise_distances):
-        '''
-        Converts the pairwise distances iterator from NetworkX to a distance matrix
-
-        First finds the attributes and generates an empty distance matrix.
-        The cells will be filled with 1 - the values of the corresponding jaccard measure.
-        Remaining cells have the distance 1
-        Returns the list of activities and the distance matrix
-        '''
-        
-        # retrieve all attribute values
-        activities = list(pm4py.get_event_attribute_values(self.log, "concept:name").keys())
-        activities.sort()
-
-        # create empty distance matrix using the activity names    
-        distance_matrix = pd.DataFrame(columns=activities, index=activities)
-
-        # fill matrix up
-        for u, v, distance in pairwise_distances:
-            distance_matrix[u][v] = 1 - round(distance, 6)
-            distance_matrix[v][u] = 1 - round(distance, 6)
-
-        distance_matrix = distance_matrix.fillna(1) # no similarity -> distance = 1
-        distance_matrix = distance_matrix.to_numpy()
-        np.fill_diagonal(distance_matrix, 0) # similarity to itself is always 0
-        
-        # return only the values of the matrix, not the names of the activities
-        return activities, distance_matrix
+        return jaccard_table
 
     def perform_clustering(self, verbose=False, ax=None, no_plot=False):
         '''
         Performs all necessary steps to perform hierarchical clustering using simple jaccard similarity
         and then generating a hierarchy used for abstracting the event log
         '''
+        # convert event log to an undirected NetworkX graph
+        dfg, start, end = pm4py.discover_dfg(self.log)
+        edge_list = [(edge[0], edge[1], weight) for edge, weight in dfg.items()]
+        self.G = nx.DiGraph()
+        self.G.add_weighted_edges_from(edge_list)
 
         # display the dfg of the given log
         if verbose:
             Log_processing.show_dfg_of_log(self.log)
 
-        # generate a distance matrix using the directly follows graph of the events
-        self.distances = self.get_jaccard_distance_of_dfg()
-        self.activities, self.distance_matrix = self.convert_distances_to_distance_matrix(self.distances)
-        
+        # convert the log to a DataFrame 
+        self.connections_df = Log_processing.get_pivot_df_from_dfg(self.log)
+        self.activities = self.connections_df.columns
+
+        # generate the distance matrix
+        self.distance_matrix = self.get_jaccard_distance_matrix(self.activities)
+
         # print the distances between events
         if verbose:
-            print(self.distance_matrix)
-
+            print(self.distance_matrix)        
+            
         # perform hierarchical clustering and generate the resulting dendrogram
-        self.linkage = Clustering.create_linkage(self.distance_matrix)
+        self.linkage = Clustering.create_linkage(self.distance_matrix.to_numpy())
         self.dendrogram = Clustering.create_dendrogram(self.activities, self.linkage, ax=ax, no_plot=no_plot)
-
-        # generate the hierarchies dictionary
-        self.clusterings = Clustering.create_clusterings_for_every_level(self.activities, self.distance_matrix, self.linkage)
-        self.hierarchies = Clustering.create_hierarchy_for_activities(self.activities, self.clusterings)
-
-        # print the hierarchies
-        if verbose:
-            Clustering.print_hierarchy_for_activities(self.hierarchies)
 
 class Weighted_Jaccard:
     def __init__(self, log):
